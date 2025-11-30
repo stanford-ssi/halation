6c26c0fb71b3a0d93b6b9296b59c5e0e20994634
Hinson Chan <hinson@stanford.edu>
2025-11-29 22:43:35 -0800

    remove ramping code

--- src/rover_control/rover_control/motor_control.py
+++ src/rover_control/rover_control/motor_control.py
@@ -9,10 +9,9 @@ import Jetson.GPIO as GPIO
 VOLTAGE_RAMP_RATE = 10  # recalculations per second
 PING_RATE = 0.1  # every n seconds
 
-REAL_VOLTAGE = 3.3 # volts
+REAL_VOLTAGE = 5 # volts
 VOLTAGE_MIN = -4095 # dac value
 VOLTAGE_MAX = 4095 # dac value
-RAMP_STEP = 100
 
 I2C_ADDRESS = 0x60
 
@@ -22,7 +21,6 @@ class MotorControl(Node):
     def __init__(self):
         super().__init__("motor_control")
         self.dac_val = 0.0
-        self.target_dac_val = 0.0
 
         # i2c = busio.I2C(board.SCL, board.SDA)
         self.dac = adafruit_mcp4725.MCP4725(i2c, address=I2C_ADDRESS)
@@ -36,28 +34,20 @@ class MotorControl(Node):
         )
 
         self.create_timer(PING_RATE, self.ping_voltage)
-        self.create_timer(1.0 / VOLTAGE_RAMP_RATE, self.ramp_voltage)
 
     def handle_target_voltage(self, msg):
-        self.target_dac_val = int((msg.data / REAL_VOLTAGE) * VOLTAGE_MAX)
-        self.get_logger().info(f"Target voltage set to {self.target_dac_val}")
+        target_dac = int((msg.data / REAL_VOLTAGE) * VOLTAGE_MAX)
+        self.get_logger().info(f"Target voltage set to {target_dac}")
+        if abs(self.dac_val - target_dac) < 0.01:
+            return
+        self.dac_val = max(min(target_dac, VOLTAGE_MAX), VOLTAGE_MIN)
+        self.set_dac_val() 
 
     def set_dac_val(self):
         # direction = GPIO.HIGH if self.dac_val > 0 else GPIO.LOW
         # GPIO.output(GPIO_CHANNEL, direction) 
         print(f"Setting to: {int(abs(self.dac_val))} {type(self.dac_val)}")
         self.dac.raw_value = int(abs(self.dac_val))
-
-    def ramp_voltage(self):
-        if abs(self.dac_val - self.target_dac_val) < 0.01:
-            return
-
-        dac_val = self.dac_val + \
-            (RAMP_STEP if self.dac_val < self.target_dac_val else - RAMP_STEP)
-
-        self.dac_val = max(min(dac_val, VOLTAGE_MAX), VOLTAGE_MIN)
-        self.set_dac_val() 
-
 
     def ping_voltage(self):
         ping_val = (self.dac_val / abs(VOLTAGE_MAX)) * REAL_VOLTAGE
