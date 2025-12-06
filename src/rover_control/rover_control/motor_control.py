c2f9f15c96500f6fdedfa7edff8c6361c6c3384e
flatypus [mac] <yhc3141@gmail.com>
2025-12-06 10:42:09 -0800

    update

--- src/rover_control/rover_control/motor_control.py
+++ src/rover_control/rover_control/motor_control.py
@@ -1,6 +1,6 @@
 from rclpy.node import Node
 from std_msgs.msg import Float32
-import adafruit_mcp4725
+import adafruit_dacx578
 import board
 import busio
 import rclpy
@@ -10,10 +10,9 @@ VOLTAGE_RAMP_RATE = 10  # recalculations per second
 PING_RATE = 0.1  # every n seconds
 
 REAL_VOLTAGE = 5 # volts
-VOLTAGE_MIN = -4095 # dac value
 VOLTAGE_MAX = 4095 # dac value
 
-I2C_ADDRESS = 0x60
+I2C_ADDRESS = 0x4c
 
 # print("PINS", { k: list(GPIO.gpio_pin_data.get_data()[-1]["TEGRA_SOC"].keys())[i] for i, k in enumerate(GPIO.gpio_pin_data.get_data()[-1]["BOARD"])})
 DIRECTION_CHANNEL = "GP167" # board pin 7
@@ -27,7 +26,7 @@ class MotorControl(Node):
         super().__init__("motor_control")
         self.dac_val = 0.0
 
-        self.dac = adafruit_mcp4725.MCP4725(i2c, address=I2C_ADDRESS)
+        self.dac = adafruit_dacx578.MCP4725(i2c, address=I2C_ADDRESS)
         self.get_logger().info(f"Motor control initialized with I2C address {I2C_ADDRESS}, {board.SCL}, {board.SDA}")
 
         self.voltage_publisher = self.create_publisher(Float32, "voltage", 10)
@@ -42,15 +41,14 @@ class MotorControl(Node):
         self.get_logger().info(f"Target voltage set to {target_dac}")
         
         if abs(self.dac_val - target_dac) < 0.01:
-            self.get_logger().info(self.dac_val, target_dac)
             return
-        self.dac_val = max(min(target_dac, VOLTAGE_MAX), VOLTAGE_MIN)
+        self.dac_val = max(min(target_dac, VOLTAGE_MAX), -VOLTAGE_MAX)
         self.set_dac_val() 
 
     def set_dac_val(self):
         self.get_logger().info(f"Setting to: {int(abs(self.dac_val))}")
         self.get_logger().info(f'Direction should be: {"HIGH" if self.dac_val >= 0 else "LOW"}')
-        self.dac.raw_value = int(abs(self.dac_val))
+        self.dac.channels[0].raw_value = int(abs(self.dac_val))
         direction = GPIO.HIGH if self.dac_val >= 0 else GPIO.LOW
         GPIO.output(DIRECTION_CHANNEL, direction) 
 
