284b3e162dee1c992a0526283164b3fcee15f25e
Hinson Chan <hinson@stanford.edu>
2025-12-13 14:20:23 -0800

    updated code

--- src/rover_sensing/rover_sensing/lidar_servo.py
+++ src/rover_sensing/rover_sensing/lidar_servo.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Float32
 import Jetson.GPIO as GPIO
 
 SERVO_PIN = 33
@@ -17,22 +16,20 @@ class ServoDriver(Node):
         self.pwm = GPIO.PWM(SERVO_PIN, PWM_FREQ)
         self.pwm.start(3.0)
 
-        self.subscription = self.create_subscription(
-            Float32, 'servo_angle', self.listener_callback, 10)
+        self.duty_low = 0
+        self.duty_high = 10
+        self.current_high = False
 
-        self.get_logger().info("Servo Node Started. Listening on /servo_angle")
+        self.timer = self.create_timer(0.1, self.sweep_callback)
 
-    def map_angle(self, angle):
-        # Constrain angle 0-180
-        angle = max(0, min(180, angle))
-        # Map 0-180 deg to 600-2400 us (FS5103B specs)
-        pulse_us = 600.0 + (angle / 180.0) * (2400.0 - 600.0)
-        return (pulse_us / 20000.0) * 100.0
+        self.get_logger().info("Servo Node Started. Sweeping up/down.")
 
-    def listener_callback(self, msg):
-        duty = self.map_angle(msg.data)
-        self.pwm.ChangeDutyCycle(duty)
-        self.get_logger().info(f'Set angle: {msg.data}')
+    def sweep_callback(self):
+        if self.current_high:
+            self.pwm.ChangeDutyCycle(self.duty_low)
+        else:
+            self.pwm.ChangeDutyCycle(self.duty_high)
+        self.current_high = not self.current_high
 
     def cleanup(self):
         self.pwm.stop()
